b8f897f1a279ad97e24860b70bb6d516d3059c63
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-04-15 15:29:04 +0200

    ipotesi di ciclo

--- index.php
+++ index.php
@@ -39,6 +39,10 @@
         ],
 
     ];
+    foreach($hotels as $hotel) {
+        $nome = $hotels['$name'];
+        $descrizione = $hotels['$description'];
+    }
 
 ?>
 
@@ -50,8 +54,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Hotel</title>
+
+  <!-- BOOLSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
 </head>
 <body>
+
+<div class="container">
+    <div>
+        <ul>
+            <?php foreach($hotels as $hotel): ?>
+                <!-- <li> <?php print_r($hotel)  ?> </li> -->
+                <li> <?php print_r ($hotels[0]['name'])  ?> bubu</li>
+                <li> <?php print_r ($hotels[$nome])  ?> bubu</li>
+            <?php endforeach; ?>     
+            <!-- <li></li>
+            <li></li>
+            <li></li>
+            <li></li> -->
+        </ul>
+    </div>
+
+</div>
   
 </body>
 </html>
